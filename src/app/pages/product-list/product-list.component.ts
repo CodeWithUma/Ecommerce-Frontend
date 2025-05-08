93b90e01668c54aceba4fc41dc4703a432253ba8
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-05-09 00:34:51 +0530

    Product List + 404

---
+++ src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+
+// This class defines an Angular component that implements OnInit lifecycle hook
+export class ProductListComponent implements OnInit {
+
+  // Declare a property named 'products' which is an array of Product objects
+  products: Product[] = [];
+
+  // The constructor injects a service of type ProductService.
+  // This is Angular's dependency injection at work, allowing the component to use the service.
+  constructor(private productService: ProductService) {}
+
+  // ngOnInit is a lifecycle hook that runs after the component is initialized.
+  // It is the best place to load data from a server.
+  ngOnInit(): void {
+    
+    // Call the getProducts method from the ProductService.
+    // This method likely returns an Observable<Product[]>.
+    // We subscribe to this Observable to receive the data when it's ready.
+    // Once the data is received, we assign it to the products array.
+    this.productService.getProducts().subscribe(data => this.products = data);
+  }
+}
+
